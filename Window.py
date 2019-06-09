dfabe59d20b41c4b4626289831390bd25c3119b3
Marcin Kostrzewski <komar86311@gmail.com>
2019-06-09 23:10:52 +0200

    Window class done

---
+++ Window.py
@@ -0,0 +1,12 @@
+DRAW_X_LIMIT = 4096
+DRAW_Y_LIMIT = 2056
+
+class Window():
+    def __init__(self):
+        self.drawingMatrix=[],[]
+    def setFrame(self):
+        pass
+    def setTitle(self):
+        pass
+    def initMatrix(self):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+DRAW_X_LIMIT = 4096
+DRAW_Y_LIMIT = 2056
+
+class Window():
+    drawingMatrix=[][]
+    
